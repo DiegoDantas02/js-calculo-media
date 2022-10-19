b00f9a7f2945221d92901b562d2bafc7ef89fb8f
Diego R Dantas <112526586+DiegoDantas02@users.noreply.github.com>
2022-10-18 21:09:53 -0300

    Update app.js
    igual ao portugol com variavel e deixar formulas iguais ao seu numero

--- app.js
+++ app.js
@@ -2,13 +2,13 @@
 // console.log(10*2)
 // console.log(10/2)
 
-let nome  = "Diego"
+let nome  = "Diego" /*Deixar nome igual a Diego*/
 let nota1 = 9.3
 let nota2 = 7.2
 let nota3 = 4.5
 let nota4 = 2.33
 let media = (nota1 + nota2 + nota3 + nota4) / 4
-let mediaFixada = media.toFixed (1)
+let mediaFixada = media.toFixed (1)/*Fixar média*/
 
 console.log("olá " +  nome)
 console.log("Sua Media foi:  " + mediaFixada)
